5ebb1abacd96722669c54dcfcddba3f203db2fb8
saitej41435 <saitej414@gmail.com>
2025-09-21 16:56:57 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,6 @@
+const header1= React.createElement('h1', {id: 'header1', key: 0 }, 'Hello World')
+const header2= React.createElement('h1', {id: 'header2', key: 1 }, 'Hello react')
+const child = React.createElement('div', {id: 'child'}, [header1, header2])
+const parent = React.createElement('div', {id: 'parent'}, child)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
